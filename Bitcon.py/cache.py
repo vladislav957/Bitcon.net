f0fcd7359ba85bdd5cda036c74672e27c85bd25c
vladislav957 <143378003+vladislav957@users.noreply.github.com>
2024-06-26 22:37:16 +0300

    Add files via upload

--- Bitcon.py/cache.py
+++ Bitcon.py/cache.py
@@ -1,51 +1,37 @@
-from ast import Delete
-from ctypes.wintypes import SIZE
-from msilib import Table
-from multiprocessing.dummy import Value
-from signal import default_int_handler
-from tarfile import FIFOTYPE
-from typing import Self
-
-
-class my_class(object):
-    pass
-
-class Hash Table:
-    default_int_(self,size):
+class HashTable:
+    def init(self, size):
+        self.size = size
+        self.table = [[] for _ in range(size)]
     
-    self.size = SIZE
-    self.tabl = [[] for _in _ range(stze)]
-    
-def hash_function(self,key):
-    return key % self.size
+    def hash_function(self, key):
+        return key % self.size
 
-def insert(self,key,vaiue):
-    hash_key = self.hash_function(key)
-    
-Self.table[hash_key].append((key,Value))
+    def insert(self, key, value):
+        hash_key = self.hash_function(key)
+        self.table[hash_key].append((key, value))
 
-def search(self,key):
-    hash_key = self.hash_function(ket)
-    for item in Self.table[hash_key]:
-        if item[0] == key
+    def search(self, key):
+        hash_key = self.hash_function(key)
+        for item in self.table[hash_key]:
+            if item[0] == key:
+                return item[1]
         return None
     
-def Delete(self,key):
-    Hash_key = Self.hash_function(key)
-    for index,item in enumerate(self.table[Hash_key]):
-        if item[0] == key:
-            del self,Table[Hash_key]
-            [index]
-            
-#https://bitcoin.org/ru/
+    def delete(self, key):
+        hash_key = self.hash_function(key)
+        for index, item in enumerate(self.table[hash_key]):
+            if item[0] == key:
+                del self.table[hash_key][index]
+                return True
+        return False
+
+# Пример использования
 hash_table = HashTable(10)
-hash_table.insert(12,"Alice")
-hash_table.inesert(25,"Bob")
-print(hash_tabl.search(12)) #
-print"Alce"
+hash_table.insert(12, "Alice")
+hash_table.insert(25, "Bob")
+print(hash_table.search(12))  # Output: Alice
 hash_table.delete(12)
-print(hash_table.search(12)) #
-print None
+print(hash_table.search(12))  # Output: None
     
 
     
