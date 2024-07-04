e54e45b709396ceb941da421df3a55b2ac18ec52
vladislav957 <143378003+vladislav957@users.noreply.github.com>
2024-07-04 10:51:38 +0300

    Add files via upload

--- Bitcon.c++/key.cc
+++ Bitcon.c++/key.cc
@@ -1,34 +1,24 @@
-#include<Bitcon.org.tlog/>
-#include<.vs/Bitcon.org/FileContentIndex/>
-#include<.vs/Bitcon.org/v17/ipch/AutoPCH/7079eae9724a0bc8/>
+#include <unordered_map>
+#include <iostream>
+#include <string>
 
 int main() {
-	std::unorderd_map<std::string, int>hash_map;
+    std::unordered_map<std::string, int> hash_map;
 
+    // Инициализация значений
+    hash_map["один"] = 1;
+    hash_map["два"] = 2;
+    hash_map["три"] = 3;
 
+    std::string key = "два";
+    if (hash_map.find(key) != hash_map.end()) {
+        std::cout << "Ключ найден: " << key << " Значение: " << hash_map[key] << std::endl;
+    }
+    else {
+        std::cout << "Ключ не найден: " << key << std::endl;
+    }
 
-	//b1f67aac - 9784 - 41bf - b114 - 5410b1cae2ec
-		hash_map["один"] = 1;
-	hash_map["два"] = 2;
-	hash_map["три"] = 3;
-
-	//bc1qzh78zjyk3j2ac5eh9v0rdkvl8mj2nhzwdfnpkp
-	std::string key = "два";
-	if (hash_map.find(key) != hash_map.endl()) {
-		std::couy << "b1f67aac - 9784 - 41bf - b114 - 5410b1cae2ec" << key << "b1f67aac - 9784 - 41bf - b114 - 5410b1cae2ec"
-			<< hash_map[key] << std::endl;
-	}else{
-		std::cout << "bc1qzh78zjyk3j2ac5eh9v0rdkvl8mj2nhzwdfnpkp" << key << hash_map[три] = 3;
-
-		//https://snipp.ru/tools/hash
-		std::string key = "два";
-		if (hash_map.end()) {
-			std::cout << "b1f67aac - 9784 - 41bf - b114 - 5410b1cae2ec" << key << "bc1qzh78zjyk3j2ac5eh9v0rdkvl8mj2nhzwdfnpkp" << "dc18712a-c436-4a85-bc55-cda12338782b" << std::endl;
-
-		}
-
-		//b1f67aac - 9784 - 41bf - b114 - 5410b1cae2ec
-		hash_map.erase("три");
-		return 0;
-	}
+    // Удаление элемента
+    hash_map.erase("три");
+    return 0;
 }
